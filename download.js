12f1f8c007f523b9fc488928723fabc32cee012a
jashanbhullar <jsbhullar96@gmail.com>
2019-05-26 05:15:57 +0530

    First Commit

---
+++ download.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
+const Multiprogress = require("multi-progress");
+const progressBars = new Multiprogress(process.stderr);
+const episodes = require("./episode-links");
+
+const DIRECTORY_NAME = "reward";
+const MAX_PARALLEL_DOWNLOADS = 5;
+const START_INDEX = +process.argv[2] - 1;
+const END_INDEX = +process.argv[3];
+
+fs.mkdirSync(DIRECTORY_NAME);
+
+if (
+  isNaN(START_INDEX) ||
+  isNaN(END_INDEX) ||
+  START_INDEX < 0 ||
+  END_INDEX < START_INDEX ||
+  END_INDEX <= 0
+) {
+  console.log("Please specify both start and end index");
+  process.exit(0);
+}
+
+(async () => {
+  const links = episodes;
+
+  if (links.length === 0) {
+    console.log("0 episodes to download, exiting ");
+  }
+
+  const linkstoDownload = links.slice(START_INDEX, END_INDEX);
+  console.log("Total links to be downloaded", linkstoDownload.length);
+
+  while (linkstoDownload.length > 0) {
+    await Promise.all(
+      linkstoDownload
+        .splice(0, MAX_PARALLEL_DOWNLOADS)
+        .map(link => download(link))
+    );
+
+    console.log(linkstoDownload.length, "downloads left");
+  }
+})();
+
+async function download({ url: link, title }) {
+  await new Promise((resolve, reject) => {
+    const bar = progressBars.newBar(`${title} [:bar]  :percent :etas`, {
+      complete: "=",
+      incomplete: " ",
+      width: 20,
+      total: 100
+    });
+    ffmpeg(link)
+      .on("error", error => {
+        reject(new Error(error));
+      })
+      .on("end", () => {
+        resolve();
+      })
+      .on("progress", progress => {
+        bar.update(progress.percent / 100);
+      })
+      .outputOptions("-c copy")
+      .outputOptions("-bsf:a aac_adtstoasc")
+      .output(`${DIRECTORY_NAME}/${title}.mp4`)
+      .run();
+  });
+}
---
+++ get-links.js
@@ -0,0 +1,59 @@
+const https = require("https");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const COURSE_URL = process.argv[2];
+
+if (!COURSE_URL) {
+  console.log("Please provide a course url.");
+  process.exit(0);
+}
+
+const getPage = url => {
+  return new Promise((resolve, reject) => {
+    https
+      .request(url, res => {
+        let responseData = "";
+
+        res.on("data", d => (responseData += d));
+
+        res.on("end", () => {
+          resolve(responseData);
+        });
+      })
+      .on("error", e => reject(e))
+      .end();
+  });
+};
+
+(async () => {
+  try {
+    const page = await getPage(COURSE_URL);
+    const $ = cheerio.load(page);
+
+    const data = JSON.parse(
+      $('script[type="application/json"][data-component-name="App"]')[0]
+        .children[0].data
+    );
+
+    const lessons = [];
+
+    for (const lesson of data.course.course.lessons) {
+      const {
+        title,
+        media_urls: { hls_url: url }
+      } = lesson;
+
+      lessons.push({
+        title,
+        url
+      });
+    }
+
+    fs.writeFileSync("episode-links.json", JSON.stringify(lessons, null, 4));
+    console.log(`File saved, total ${lessons.length} episodes to download`);
+  } catch (err) {
+    console.log("Downloading of the urls failed");
+    throw err;
+  }
+})();
